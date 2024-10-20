ce54ffd2f21e04ad672010c4133e6b45458eb70e
Sameer chauhan <96983605+sameerchauhan360@users.noreply.github.com>
2024-10-20 16:51:59 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle the navigation menu height
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    const nav = document.getElementById("nav-links");
+
+    // Toggle the class to expand or collapse the nav
+    nav.classList.toggle("nav-open");
+});
